86224a627f1ee270df3fa653242928d496650f17
faisalrehmani <fh1632727@gmail.com>
2024-01-01 05:13:33 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,126 @@
+// let form = document.querySelector("form");
+// let select = document.querySelector("select");
+// let textarea = document.querySelector("textarea");
+// let ul = document.querySelector("ul");
+
+
+// const saveFeedback = (e) => {
+//     e.preventDefault();
+
+//     let li = document.createElement("li");
+//     let h1 = document.createElement("h1");
+//     let h2 = document.createElement("h2");
+//     let delBtn = document.createElement("button");
+
+//     li.className = "list-group-item rounded-0";
+//     h1.className = "displat-3";
+//     h2.className = "display-4";
+//     delBtn.className = "btn btn-danger rounded-0 float-end";
+
+//     h1.innerText = "Rating : " + select.value;
+//     h2.innerText = "Review : " + select.value;
+//     delBtn.innerText = "Delete";
+
+
+
+//     li.appendChild(h1);
+//     li.appendChild(h2);
+//     li.appendChild(delBtn);
+
+//     ul.appendChild(li);
+ 
+ 
+//     form.reset;
+// };
+
+// form.addEventListener("submit", saveFeedback);
+
+
+
+let form = document.querySelector("form");
+let select = document.querySelector("select");
+let textarea = document.querySelector("textarea");
+let ul = document.querySelector("ul");
+
+const saveFeedback = (e) => {
+    e.preventDefault();
+
+let li = document.createElement("li");
+let h1 = document.createElement("h1");
+let h2 = document.createElement("h2");
+let delBtn = document.createElement("button");
+
+li.className = "list-group-item rounded-0 shadow-sm";
+h1.className = "display-3";
+h2.className = "display-4";
+delBtn.className = "btn btn-sm btn-danger rounded-0 float-end";
+
+h1.innerText = "Rating : " + select.value;
+h2.innerText = "Review : " + textarea.value
+;
+delBtn.innerText = "Delete";
+
+li.appendChild(h1);
+li.appendChild(h2);
+li.appendChild(delBtn);
+ul.appendChild(li);
+form.reset();
+}
+
+form.addEventListener("submit", saveFeedback);
+
+
+// const deleteTodo = (e) => {
+//         if (e.target.className.includes("btn-danger")) {
+//           let li = e.target.parentElement;
+//           if (window.confirm("Are You Sure?")) {
+//             ul.removeChild(li);
+//           }
+//     }
+
+// };
+// ul.addEventListener("click", deleteTodo);
+
+
+const deleteTodo = (e) => {
+    if (e.target.className.includes("btn-danger")) {
+        let li = e.target.parentElement; 
+
+       if (window.confirm("Are You Sure?")){
+        ul.removeChild(li);
+       }
+        
+    }
+};
+ul.addEventListener("click", deleteTodo);
+
+
+
+// let clearBtn = document.querySelector("#clearBtn");
+
+// clearBtn.addEventListener("click", () => {
+//     let allListItems = document.querySelectorAll("li");
+
+//     allListItems.forEach((item) => {
+//         if (window.confirm("Are You Sure?")) {
+
+//             ul.removeChild(item);
+//         }
+//     });
+// });
+
+ let clearBtn = document.querySelector("#clearBtn");
+
+ clearBtn.addEventListener("click", () => {
+    let allListItems = document.querySelectorAll("li");
+    allListItems.forEach((item) => {
+        ul.removeChild(item);
+    });
+ });
+
+
+
+
+
+
+
